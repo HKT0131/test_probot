52842223e4dc7494d1e07dececfbf4b1383c7257
HKT0131 <e175414y@st.u-gakugei.ac.jp>
2020-10-11 16:24:18 +0900

    fix feature
    順序を整理して2重コメントの問題を解消
    コメントを参照できるようになればなおよい

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,29 @@ import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
   // Pull Requestが作成されたタイミングで呼び出されます
-  app.on('issues.labeled', async (context : any) => {
 
+  //need_checkラベル張る
+  app.on('issues.labeled', async (context : any) => {
     const isBug = context.payload.issue.labels.some((label: any) => { return label.name === 'bug' })
-    
-    //Bugならコメントつける
     if(isBug){
       // ラベル追加APIを呼び出す際のリクエストパラメータ生成
       // Returns: {owner: 'username', repo: 'reponame', number: 123, labels: ['レビュー可能']}
       const params = context.issue({
         labels: ['need_check']
       })
-
       // 作成されたPull Requestにラベル追加
       await context.github.issues.addLabels(params)
     }
   })
 
+  //need_checkならコメントつける
   app.on('issues.labeled', async(context : any) => {
     // ラベルの文字列で条件分岐
-    const isBug = context.payload.issue.labels.some((label: any) => { return label.name === 'bug' })
-    
-    //Bugならコメントつける
+    const isBug = context.payload.issue.labels.some((label: any) => { return label.name === 'need_check' })
     if(isBug){
       const params = context.issue({
         body : 'Please look back this bug!!\nhttps://docs.google.com/forms/d/e/1FAIpQLSdufxjUrGALB-aBinIiWBYRS2KVNazSCKNK1JweVCQV3itGuw/viewform'
       })
-
       await context.github.issues.createComment(params)
     }
   })
